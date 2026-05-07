c35752a7502275b564a22fa70e6b9a6d9cb8119b
yeong3820 <yeongmin7723@naver.com>
2026-05-07 15:49:59 +0900

    10주차

--- routes/haksa.js
+++ routes/haksa.js
@@ -8,12 +8,30 @@ router.get('/pro', function(req, res, next) {
   res.render('index', {title:'교수관리', pageName:'haksa/professors.ejs'});
 });
 
+// 교수등록 페이지
+router.get('/pro/insert', async function(req, res) {
+  let code;
+  let con;
+  try{
+    con = await getConnection();
+    const sql="select max(pcode)+1 from professors";
+    const result=await con.execute(sql);
+    code = result.rows[0][0];
+  }catch(err){
+
+  }finally{
+    if(con) await con.close();
+  }
+  res.render('index.ejs', {title:'교수등록', pageName:'haksa/professors_insert.ejs', code});
+});
+
 /* 교수 데이터 생성 */
 router.get('/pro/list.json', async function(req, res) {
     var con;
     try{
         con = await getConnection();
-        const sql="select * from professors";
+        let sql="select p.*, to_char(hiredate, 'YYYY-MM-DD') fdate, to_char(salary, '99,999,999') fsalary from professors p";
+        sql += " order by pcode";
         const result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT}); 
         res.send(result.rows);
     }catch(err){
@@ -21,6 +39,49 @@ router.get('/pro/list.json', async function(req, res) {
     }finally{
         if(con) await con.close();
     }
+});
+
+
+//교수등록
+router.post('/pro/insert', async function(req, res) {
+  console.log('교수등록');
+  const pcode=req.body.pcode;
+  const pname=req.body.pname;
+  const dept=req.body.dept;
+  const title=req.body.title;
+  const hiredate=req.body.hiredate;
+  const salary=req.body.salary;
+  console.log(pcode, pname, dept, title, hiredate, salary);
+  let con;
+  try{
+    con = await getConnection();
+    let sql="insert into professors(pcode,pname,dept,title,hiredate,salary) "; 
+    sql += "values(:pcode,:pname,:dept,:title,:hiredate,:salary)";
+    await con.execute(sql, {pcode, pname, dept, title, hiredate, salary},{autoCommit:true});
+  }catch(err){
+    
+  }finally{
+    if(con) await con.close();
+  }
+
+  res.sendStatus(200);
+});
+
+//교수삭제 REST API
+router.post('/pro/delete', async function(req, res) {
+  const pcode=req.body.pcode;
+  console.log(pcode);
+  try{
+    con = await getConnection();
+    let sql="delete from professors where pcode=:pcode"; 
+    await con.execute(sql, {pcode},{autoCommit:true});
+    res.sendStatus(200);
+  }catch(err){
+    console.log(err);
+    res.sendStatus(500);
+  }finally{
+    if(con) await con.close();
+  }
 });
 
 /* 학생 관리 페이지 */
