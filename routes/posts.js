716a613da4f546e305b92c209c2d7b1196a33020
yeong3820 <yeongmin7723@naver.com>
2026-05-21 15:44:48 +0900

    12주 수정 요망

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var {getConnection} = require('./connect');
+var oracledb = require('oracledb');
 
 /* 게시판 */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '게시판', pageName:'posts/list.ejs'});
 });
 
+// 게시글 목록 데이터
+router.get('/list.json', async function(req, res) {
+  let con;
+  const page=parseInt(req.query.page);
+ const size=parseInt(req.query.size);
+  const off_row=(page-1)*size;
+  try{
+    con = await getConnection();
+    let sql = "select * from posts view_posts order by id desc ";
+      sql += `offset ${off_row} rows fetch next ${size} rows only`;
+    let result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+    const list = result.rows;
+
+    sql = "select conut(*) from posts";
+    result = await con.execute(sql);
+    const count=result.rows[0][0];
+    console.log(result.rows);
+    res.send(result.rows);
+  }catch(err){
+    console.log("게시글 목록 데이터", err.message);
+    res.status(500).send(err.message);
+  }finally{
+    if(con) await con.close();
+  }
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var {getConnection} = require('./connect');
+var oracledb = require('oracledb');
 
 /* 로그인 페이지 */
 router.get('/login', function(req, res, next) {
   res.render('index', {title:'로그인', pageName:'login.ejs'});
 });
 
+//로그인체크
+router.post('/login', async function(req, res) {
+  const scode = req.body.scode;
+  let con;
+  try{
+    con = await getConnection();
+    let sql = "select * from students where scode=:scode";
+    let result = await con.execute(sql, {scode}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+    console.log(result.rows[0]);
+    res.send(result.rows[0]);
+  }catch(err){
+    console.error("로그인체크", err.message);       // 시험 나올 수 있음
+  }finally{
+    if(con) await con.close();
+  }
+});
 module.exports = router;
